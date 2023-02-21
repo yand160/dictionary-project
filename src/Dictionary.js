25d4b10b6823f0b8f03ade837a80386fc813a8a8
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-21 17:53:05 -0500

    added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={7eo5ded69c3ffa8b7taebbf0b44b9fb2}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
