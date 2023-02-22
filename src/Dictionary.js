0ec1dfd38ec0050779374366f2e40298888594c5
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-21 22:06:09 -0500

    fixed apiUrl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,17 +4,16 @@ import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState(null);
+  const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word={word}&key={7eo5ded69c3ffa8b7taebbf0b44b9fb2}`;
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key={7eo5ded69c3ffa8b7taebbf0b44b9fb2}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
