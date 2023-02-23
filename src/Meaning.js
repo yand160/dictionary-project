b349c5fd6010491fda08d3056931b7cb15a154ea
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-23 18:37:53 -0500

    fixed api fetching

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,15 +4,8 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
-          </div>
-        );
-      })}
+      <div className="definition">{props.meaning.definitions}</div>
+      <div className="example">{props.meaning.example}</div>
     </div>
   );
 }
