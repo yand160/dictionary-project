a081f6f1144c4b8353321e0c6bdf0d873bc22dbf
yand160 <74022340+yand160@users.noreply.github.com>
2023-02-27 17:15:57 -0500

    removed phonetic

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
@@ -9,13 +8,6 @@ export default function Results(props) {
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
-          {props.results.phonetic.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
