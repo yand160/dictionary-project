a933ef918abbd24c46ec5c2519e216e4e8c55da9
yand160 <74022340+yand160@users.noreply.github.com>
2023-03-04 18:03:03 -0500

    updated css styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
       <ul className="Synonyms">
-        {props.synonyms.map(function (synonyms, index) {
-          return <li key={index}>{synonyms}</li>;
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
         })}
       </ul>
     );
