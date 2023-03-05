b48b5db7774df5325bbb7842e3b1c15aa47eb47c
yand160 <74022340+yand160@users.noreply.github.com>
2023-03-05 18:22:59 -0500

    updated photo js netlify error

--- src/Photos.js
+++ src/Photos.js
@@ -17,7 +17,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="{photo.src.photographer}"
+                    alt={photo.src.photographer}
                   />
                 </a>
               </div>
